6492a17f9961e77d95d0f4814816b87f993bdc59
manoj khatokar <43496884+manojvkhatokar@users.noreply.github.com>
2019-04-27 14:53:12 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,515 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 26 07:56:50 2019
+
+@author: manoj
+"""
+
+from __future__ import print_function
+import pandas as pd
+import re
+from nltk.corpus import stopwords
+from pickle import dump, load
+
+
+from keras.models import Model
+from keras.layers import Input, LSTM, Dense
+import numpy as np
+import io
+from pickle import load
+
+import nltk
+from random import randint
+from numpy import array
+from numpy import argmax
+from numpy import array_equal
+from keras.utils import to_categorical
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import LSTM
+from keras.layers import Dense
+
+reviews = pd.read_csv("reviews1000.csv")
+print(reviews.shape)
+print(reviews.head())
+print(reviews.isnull().sum())
+
+
+reviews = reviews.dropna()
+reviews = reviews.drop(['Id','ProductId','UserId','ProfileName','HelpfulnessNumerator','HelpfulnessDenominator',
+                        'Score','Time'], 1)
+reviews = reviews.reset_index(drop=True)
+
+print(reviews.head())
+for i in range(5):
+    print("Review #",i+1)
+    print(reviews.Summary[i])
+    print(reviews.Text[i])
+    print()
+
+
+contractions = {
+"ain't": "am not",
+"aren't": "are not",
+"can't": "cannot",
+"can't've": "cannot have",
+"'cause": "because",
+"could've": "could have",
+"couldn't": "could not",
+"couldn't've": "could not have",
+"didn't": "did not",
+"doesn't": "does not",
+"don't": "do not",
+"hadn't": "had not",
+"hadn't've": "had not have",
+"hasn't": "has not",
+"haven't": "have not",
+"he'd": "he would",
+"he'd've": "he would have",
+"he'll": "he will",
+"he's": "he is",
+"how'd": "how did",
+"how'll": "how will",
+"how's": "how is",
+"i'd": "i would",
+"i'll": "i will",
+"i'm": "i am",
+"i've": "i have",
+"isn't": "is not",
+"it'd": "it would",
+"it'll": "it will",
+"it's": "it is",
+"let's": "let us",
+"ma'am": "madam",
+"mayn't": "may not",
+"might've": "might have",
+"mightn't": "might not",
+"must've": "must have",
+"mustn't": "must not",
+"needn't": "need not",
+"oughtn't": "ought not",
+"shan't": "shall not",
+"sha'n't": "shall not",
+"she'd": "she would",
+"she'll": "she will",
+"she's": "she is",
+"should've": "should have",
+"shouldn't": "should not",
+"that'd": "that would",
+"that's": "that is",
+"there'd": "there had",
+"there's": "there is",
+"they'd": "they would",
+"they'll": "they will",
+"they're": "they are",
+"they've": "they have",
+"wasn't": "was not",
+"we'd": "we would",
+"we'll": "we will",
+"we're": "we are",
+"we've": "we have",
+"weren't": "were not",
+"what'll": "what will",
+"what're": "what are",
+"what's": "what is",
+"what've": "what have",
+"where'd": "where did",
+"where's": "where is",
+"who'll": "who will",
+"who's": "who is",
+"won't": "will not",
+"wouldn't": "would not",
+"you'd": "you would",
+"you'll": "you will",
+"you're": "you are"
+}
+
+
+def clean_text(text, remove_stopwords=True):
+    '''Remove unwanted characters, stopwords, and format the text to create fewer nulls word embeddings'''
+
+    # Convert words to lower case
+    text = text.lower()
+
+    # Replace contractions with their longer forms
+    if True:
+        text = text.split()
+        new_text = []
+        for word in text:
+            if word in contractions:
+                new_text.append(contractions[word])
+            else:
+                new_text.append(word)
+        text = " ".join(new_text)
+
+    # Format words and remove unwanted characters
+    text = re.sub(r'https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+    text = re.sub(r'\<a href', ' ', text)
+    text = re.sub(r'&amp;', '', text)
+    text = re.sub(r'[_"\-;%()|+&=*%.,!?:#$@\[\]/]', ' ', text)
+    text = re.sub(r'<br />', ' ', text)
+    text = re.sub(r'\'', ' ', text)
+
+    # Optionally, remove stop words
+    if remove_stopwords:
+        text = text.split()
+        stops = set(stopwords.words("english"))
+        text = [w for w in text if not w in stops]
+        text = " ".join(text)
+
+    return text
+
+
+nltk.download('stopwords')
+
+# Clean the summaries and texts
+clean_summaries = []
+for summary in reviews.Summary:
+    clean_summaries.append(clean_text(summary, remove_stopwords=False))
+print("Summaries are complete.")
+
+clean_texts = []
+for text in reviews.Text:
+    clean_texts.append(clean_text(text))
+print("Texts are complete.")
+
+stories = list()
+for i, text in enumerate(clean_texts):
+        stories.append({'story': text, 'highlights': clean_summaries[i]})
+
+# save to file
+dump(stories, open('review_dataset.pkl', 'wb'))
+
+
+'''Sequence to sequence example in Keras (character-level).
+This script demonstrates how to implement a basic character-level
+sequence-to-sequence model. We apply it to translating
+short English sentences into short French sentences,
+character-by-character. Note that it is fairly unusual to
+do character-level machine translation, as word-level
+models are more common in this domain.
+# Summary of the algorithm
+- We start with input sequences from a domain (e.g. English sentences)
+    and corresponding target sequences from another domain
+    (e.g. French sentences).
+- An encoder LSTM turns input sequences to 2 state vectors
+    (we keep the last LSTM state and discard the outputs).
+- A decoder LSTM is trained to turn the target sequences into
+    the same sequence but offset by one timestep in the future,
+    a training process called "teacher forcing" in this context.
+    Is uses as initial state the state vectors from the encoder.
+    Effectively, the decoder learns to generate `targets[t+1...]`
+    given `targets[...t]`, conditioned on the input sequence.
+- In inference mode, when we want to decode unknown input sequences, we:
+    - Encode the input sequence into state vectors
+    - Start with a target sequence of size 1
+        (just the start-of-sequence character)
+    - Feed the state vectors and 1-char target sequence
+        to the decoder to produce predictions for the next character
+    - Sample the next character using these predictions
+        (we simply use argmax).
+    - Append the sampled character to the target sequence
+    - Repeat until we generate the end-of-sequence character or we
+        hit the character limit.
+# Data download
+English to French sentence pairs.
+http://www.manythings.org/anki/fra-eng.zip
+Lots of neat sentence pairs datasets can be found at:
+http://www.manythings.org/anki/
+# References
+- Sequence to Sequence Learning with Neural Networks
+    https://arxiv.org/abs/1409.3215
+- Learning Phrase Representations using
+    RNN Encoder-Decoder for Statistical Machine Translation
+    https://arxiv.org/abs/1406.1078
+'''
+
+
+
+batch_size = 64  # Batch size for training.
+epochs = 30  # Number of epochs to train for.
+latent_dim = 256  # Latent dimensionality of the encoding space.
+num_samples = 10000  # Number of samples to train on.
+# Path to the data txt file on disk.
+
+stories = load(open('review_dataset.pkl', 'rb'))
+print('Loaded Stories %d' % len(stories))
+print(type(stories))
+
+# Vectorize the data.
+input_texts = []
+target_texts = []
+input_characters = set()
+target_characters = set()
+for story in stories:
+    input_text = story['story']
+    for highlight in story['highlights']:
+        target_text = highlight
+
+    # We use "tab" as the "start sequence" character
+    # for the targets, and "\n" as "end sequence" character.
+    target_text = '\t' + target_text + '\n'
+    input_texts.append(input_text)
+    target_texts.append(target_text)
+    for char in input_text:
+        if char not in input_characters:
+            input_characters.add(char)
+    for char in target_text:
+        if char not in target_characters:
+            target_characters.add(char)
+
+input_characters = sorted(list(input_characters))
+target_characters = sorted(list(target_characters))
+num_encoder_tokens = len(input_characters)
+num_decoder_tokens = len(target_characters)
+max_encoder_seq_length = max([len(txt) for txt in input_texts])
+max_decoder_seq_length = max([len(txt) for txt in target_texts])
+
+print('Number of samples:', len(input_texts))
+print('Number of unique input tokens:', num_encoder_tokens)
+print('Number of unique output tokens:', num_decoder_tokens)
+print('Max sequence length for inputs:', max_encoder_seq_length)
+print('Max sequence length for outputs:', max_decoder_seq_length)
+
+input_token_index = dict(
+    [(char, i) for i, char in enumerate(input_characters)])
+target_token_index = dict(
+    [(char, i) for i, char in enumerate(target_characters)])
+
+encoder_input_data = np.zeros(
+    (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+    dtype='float32')
+decoder_input_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+decoder_target_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+
+for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+    for t, char in enumerate(input_text):
+        encoder_input_data[i, t, input_token_index[char]] = 1.
+    for t, char in enumerate(target_text):
+        # decoder_target_data is ahead of decoder_input_data by one timestep
+        decoder_input_data[i, t, target_token_index[char]] = 1.
+        if t > 0:
+            # decoder_target_data will be ahead by one timestep
+            # and will not include the start character.
+            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+
+# Define an input sequence and process it.
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder = LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+# We discard `encoder_outputs` and only keep the states.
+encoder_states = [state_h, state_c]
+
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+# We set up our decoder to return full output sequences,
+# and to return internal states as well. We don't use the
+# return states in the training model, but we will use them in inference.
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+                                     initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+# Define the model that will turn
+# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+# Run training
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_split=0.2)
+# Save model
+model.save('model2.h5')
+
+# Next: inference mode (sampling).
+# Here's the drill:
+# 1) encode input and retrieve initial decoder state
+# 2) run one step of decoder with this initial state
+# and a "start of sequence" token as target.
+# Output will be the next target token
+# 3) Repeat with the current target token and current states
+
+# Define sampling models
+encoder_model = Model(encoder_inputs, encoder_states)
+
+decoder_state_input_h = Input(shape=(latent_dim,))
+decoder_state_input_c = Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+decoder_outputs, state_h, state_c = decoder_lstm(
+    decoder_inputs, initial_state=decoder_states_inputs)
+decoder_states = [state_h, state_c]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_model = Model(
+    [decoder_inputs] + decoder_states_inputs,
+    [decoder_outputs] + decoder_states)
+
+# Reverse-lookup token index to decode sequences back to
+# something readable.
+reverse_input_char_index = dict(
+    (i, char) for char, i in input_token_index.items())
+reverse_target_char_index = dict(
+    (i, char) for char, i in target_token_index.items())
+
+
+def decode_sequence(input_seq):
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(input_seq)
+
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1, 1, num_decoder_tokens))
+    # Populate the first character of target sequence with the start character.
+    target_seq[0, 0, target_token_index['\t']] = 1.
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict(
+            [target_seq] + states_value)
+
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_char = reverse_target_char_index[sampled_token_index]
+        decoded_sentence += sampled_char
+
+        # Exit condition: either hit max length
+        # or find stop character.
+        if (sampled_char == '\n' or
+           len(decoded_sentence) > max_decoder_seq_length):
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1, 1, num_decoder_tokens))
+        target_seq[0, 0, sampled_token_index] = 1.
+
+        # Update states
+        states_value = [h, c]
+
+    return decoded_sentence
+
+
+for seq_index in range(100):
+    # Take one sequence (part of the training set)
+    # for trying out decoding.
+    input_seq = encoder_input_data[seq_index: seq_index + 1]
+    decoded_sentence = decode_sequence(input_seq)
+    print('-')
+    print('Input sentence:', input_texts[seq_index])
+    print('Decoded sentence:', decoded_sentence)
+    
+
+
+
+# generate a sequence of random integers
+def generate_sequence(length, n_unique):
+	return [randint(1, n_unique-1) for _ in range(length)]
+
+
+# prepare data for the LSTM
+def get_dataset(n_in, n_out, cardinality, n_samples):
+	X1, X2, y = list(), list(), list()
+	for _ in range(n_samples):
+		# generate source sequence
+		source = generate_sequence(n_in, cardinality)
+		# define padded target sequence
+		target = source[:n_out]
+		target.reverse()
+		# create padded input target sequence
+		target_in = [0] + target[:-1]
+		# encode
+		src_encoded = to_categorical([source], num_classes=cardinality)[0]
+		tar_encoded = to_categorical([target], num_classes=cardinality)[0]
+		tar2_encoded = to_categorical([target_in], num_classes=cardinality)[0]
+		# store
+		X1.append(src_encoded)
+		X2.append(tar2_encoded)
+		y.append(tar_encoded)
+	return array(X1), array(X2), array(y)
+
+
+# returns train, inference_encoder and inference_decoder models
+def define_models(n_input, n_output, n_units):
+	# define training encoder
+	encoder_inputs = Input(shape=(None, n_input))
+	encoder = LSTM(n_units, return_state=True)
+	encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+	encoder_states = [state_h, state_c]
+	# define training decoder
+	decoder_inputs = Input(shape=(None, n_output))
+	decoder_lstm = LSTM(n_units, return_sequences=True, return_state=True)
+	decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+	decoder_dense = Dense(n_output, activation='softmax')
+	decoder_outputs = decoder_dense(decoder_outputs)
+	model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+	# define inference encoder
+	encoder_model = Model(encoder_inputs, encoder_states)
+	# define inference decoder
+	decoder_state_input_h = Input(shape=(n_units,))
+	decoder_state_input_c = Input(shape=(n_units,))
+	decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+	decoder_outputs, state_h, state_c = decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
+	decoder_states = [state_h, state_c]
+	decoder_outputs = decoder_dense(decoder_outputs)
+	decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states)
+	# return all models
+	return model, encoder_model, decoder_model
+
+
+# generate target given source sequence
+def predict_sequence(infenc, infdec, source, n_steps, cardinality):
+	# encode
+	state = infenc.predict(source)
+	# start of sequence input
+	target_seq = array([0.0 for _ in range(cardinality)]).reshape(1, 1, cardinality)
+	# collect predictions
+	output = list()
+	for t in range(n_steps):
+		# predict next char
+		yhat, h, c = infdec.predict([target_seq] + state)
+		# store prediction
+		output.append(yhat[0,0,:])
+		# update state
+		state = [h, c]
+		# update target sequence
+		target_seq = yhat
+	return array(output)
+
+
+# decode a one hot encoded string
+def one_hot_decode(encoded_seq):
+	return [argmax(vector) for vector in encoded_seq]
+
+
+# configure problem
+n_features = 50 + 1
+n_steps_in = 6
+n_steps_out = 3
+# define model
+train, infenc, infdec = define_models(n_features, n_features, 128)
+train.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+# generate training dataset
+X1, X2, y = get_dataset(n_steps_in, n_steps_out, n_features, 100000)
+print(X1.shape,X2.shape,y.shape)
+# train model
+train.fit([X1, X2], y, epochs=1)
+# evaluate LSTM
+total, correct = 100, 0
+for _ in range(total):
+	X1, X2, y = get_dataset(n_steps_in, n_steps_out, n_features, 1)
+	target = predict_sequence(infenc, infdec, X1, n_steps_out, n_features)
+	if array_equal(one_hot_decode(y[0]), one_hot_decode(target)):
+		correct += 1
+print('Accuracy: %.2f%%' % (float(correct)/float(total)*100.0))
+# spot check some examples
+for _ in range(10):
+	X1, X2, y = get_dataset(n_steps_in, n_steps_out, n_features, 1)
+	target = predict_sequence(infenc, infdec, X1, n_steps_out, n_features)
+print('X=%s y=%s, yhat=%s' % (one_hot_decode(X1[0]), one_hot_decode(y[0]), one_hot_decode(target)))
